9f55d19dc68044e0fb1f07be602e33a8c6efc593
carole tierney <caroleatierney@gmail.com>
2023-08-23 15:13:18 -0400

    Almost MVP - if statements wrong

--- app.js
+++ app.js
@@ -1,75 +1,99 @@
-// $("button-r1c1").hide();
-
-// can you just use button for class?
 const player1='X';
 const player2='O';
-let winner = false;
-
-let clickedCells = [];
 
 var player = player1;
-
 $("#currentPlayer").text("Player 1");
 
-playGame(player);
-
-// $("#currentPlayer").text("player test");
-
-// current player
-// the winner is
-// or tie
-// button to clear all values
+const $buttons = $(".btn-square-md")
+$buttons.on("click", (event) => {
+    const $clickedButton = $(event.target);
+    $clickedButton.prop("disabled", true);
+    $clickedButton.text(player);
+    playGame(player);
+});
 
 function playGame(player) {
-
-
-    let i = 1;
-    const $buttons = $("button")
+    var winner = false;
+    const $buttons = $(".btn-square-md")
     $buttons.on("click", (event) => {
-
-        // console.log("event", event);
         const $clickedButton = $(event.target);
         $clickedButton.prop("disabled", true);
         $clickedButton.text(player);
-        // console.log('clicked text', $clickedButton.text)
-
-        console.log(("CURRENT", $("#currentPlayer").text));
-
-        if ($("#currentPlayer").text === "Player 1") {
-        // if (newPlayer === player1) {
-            $("#currentPlayer").text("Player 2");
-        } else {
-            $("#currentPlayer").text("Player 1");
-        }
-
-
-        player = isThereAWinner(player)
-        // alert(player)
-        if (winner === true) {
-            // show winner and leave
-        }
     });
-}
 
-function isThereAWinner(player) {
-
-// Create the array of winners
-
-    // console.log($("#button-r1c1").val());
-
-    // var email = $("#button-r1c1").val();
-    // alert(email);
-
-    // console.log($("#button-r1c2").text)
-    // console.log($("#button-r1c3").val())
-    // console.log($('#email').val());
-
-    if ($("#button-r1c1") === $("#button-r1c2") === $("#button-r1c3")) {
-        //  "(winner === true) {
-        // return winner = false;
-        alert("the winner is")
-        return winner
+    if ($("#currentPlayer").text() === "Player 1") {
+        player = player2
+        $("#currentPlayer").text("Player 2");
     } else {
-        return newPlayer = (player === player1) ? player2 : player1;
+        $("#currentPlayer").text("Player 1");
+        player = player1
     }
-}
+   
+    // row 1
+    if (($("#r1c1").text() !== " ") && 
+        ($("#r1c1").text() === $("#r1c2").text() && $("#r1c2").text() === $("#r1c3").text())) {
+        winner = $("#r1c1").text()
+        return winner;
+    // row 2
+    } else 
+    if ($("#r2c1").text() !== " " &&
+        ($("#r2c1").text() === $("#r2c2").text() && $("#r2c2").text() === $("#r2c3").text())) {
+        winner = $("#r2c1").text()
+        return winner;
+    // row 3
+    } else 
+    if ($("#r3c1").text() !== " " &&
+        ($("#r3c1").text() === $("#r3c2").text() && $("#3c2").text() === $("#r3c3").text())) {
+        winner = $("#r3c1").text()
+        return winner;
+    // col 1
+    } else
+    if ($("#r1c1").text() !== " " &&
+        ($("#r1c1").text() === $("#r2c1").text() && $("#r2c1").text() === $("#r3c1").text())) {
+        winner = $("#r1c1").text()
+        return winner;
+    // col 2
+    } else
+        if ($("#r1c2").text() !== " " &&
+            ($("#r1c2").text() === $("#r2c2").text() && $("#r2c2").text() === $("#r3c2").text())) {
+            winner = $("#r1c2").text()
+            return winner;
+    // col 3
+    } else
+        if ($("#r1c3").text() !== " " &&
+            ($("#r1c3").text() === $("#r2c3").text() && $("#r2c3").text() === $("#r3c3").text())) {
+            winner = $("#r1c3").text()
+            return winner;
+    // diagonal top-left bottom-right
+    } else
+        if ($("#r1c1").text() !== " " &&
+            ($("#r1c1").text() === $("#r2c2").text() && $("#r2c2").text() === $("#r3c1").text())) {
+            winner = $("#r1c1").text()
+            return winner;
+    // diagonal bottom-left top-right
+    } else
+        if ($("#r1c3").text() !== " " &&
+            ($("#r1c3").text() === $("#r2c2").text() && $("#r2c2").text() === $("#r1c3").text())) {
+            winner = $("#r1c3").text()
+            return winner;
+    // no winner
+    } else  
+            if ($("#r1c1").text() && $("#r1c2").text() && $("#r1c3"), $("#r2c1").text() && $("#r2c2").text() && $("#r2c3").text() && $("#r3c1").text() && $("#r3c2").text() && $("#r3c3").text() !== " ") {
+        winner = "tie"
+        return winner;
+    }
+
+    if (winner === "X") {
+        alert("Player 1 Wins!")
+    } else if (winner = "O") {
+        alert("Player 2 Wins!")
+    } else if (winner="tie") {
+        alert("There was a tie!")
+    }
+ 
+};
+
+$("#clear").on("click", (event) => {
+    $(".btn-square-md").text("");
+    $(".btn-square-md").prop("disabled", false);
+});
