330e4633f87580caa2db9b53a5c31d3cda3d0082
carole tierney <caroleatierney@gmail.com>
2023-08-24 09:52:59 -0400

    MVP done

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const player1='X';
+// const player1='X';
 const player2='O';
+
+const img = document.querySelector("img");
+img.src = "./images/Jocie";
+const player1 = img;
 
 let winner = false;
 let player = player1;
+
 $("#currentPlayer").text("Player 1");
 
 const $buttons = $(".btn-square-md")
@@ -20,20 +25,27 @@ $buttons.on("click", (event) => {
     let r3c2 = $("#button-r3c2").text();
     let r3c3 = $("#button-r3c3").text();
     playGame(player, r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3)
+    // display winner or tie
+    if (winner === "X") {
+        $("#currentPlayer").text("Player 1 Wins!");
+        $(".btn-square-md").prop("disabled", true);
+    } else if (winner === "O") {
+        $("#currentPlayer").text("Player 2 Wins!");
+        $(".btn-square-md").prop("disabled", true);
+    } else if (winner === "tie") {
+        $("#currentPlayer").text("There was a tie!");
+        $(".btn-square-md").prop("disabled", true);
+    } else if ($("#currentPlayer").text() === "Player 1") {
+        $("#currentPlayer").text("Player 2");
+        player = player2;
+    } else {
+        $("#currentPlayer").text("Player 1");
+        player = player1
+    }
 });
 
 function playGame(player,r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3) {
 
-    //  If no winners, continue to switch player turns
-    if ($("#currentPlayer").text() === "Player 1") {
-        $("#currentPlayer").text("Player 2");
-        let newPlayer = player2;
-        return newPlayer;
-    } else {
-        $("#currentPlayer").text("Player 1");
-        let newPlayer = player1;
-        return newPlayer;
-    }
     // row 1
     if ((r1c1 !== " ") && (r1c1 === r1c2 && r1c2 === r1c3)) {
         winner = r1c1
@@ -63,14 +75,14 @@ function playGame(player,r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3) {
         winner = "tie"
     }
 
-    // display winner or tie
-    if (winner === "X") {
-        return alert("Player 1 Wins!")
-    } else if (winner === "O") {
-        return alert("Player 2 Wins!")
-    } else if (winner === "tie") {
-        return alert("There was a tie!")
-    }
+    // // display winner or tie
+    // if (winner === "X") {
+    //    return $("#currentPlayer").text("Player 1 Wins!");
+    // } else if (winner === "O") {
+    //     return $("#currentPlayer").text("Player 2 Wins!");
+    // } else if (winner === "tie") {
+    //     return $("#currentPlayer").text("There was a tie!");
+    // }
 
 };
 
